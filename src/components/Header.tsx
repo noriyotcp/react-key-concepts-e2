5ccf8699df8caa28179c2ba4531a816e10c8c1ca
noriyotcp <noriyo.akita@gmail.com>
2024-11-04 02:31:04 +0900

    Activity 2.2 Refactor components and styles
    - Create components
    - Extract header component to separate file
    - Create goal list component
    - Add first, second, and third goal components
    - Update styles for header, main, and list items

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import myicon from "../assets/avatar-on-GH.png";
+
+function Header() {
+  return (
+    <header>
+      <img src={myicon} alt="An image of Noriyo Akita"></img>
+      <h1>Noriyo Akita</h1>
+    </header>
+  );
+}
+
+export default Header;
